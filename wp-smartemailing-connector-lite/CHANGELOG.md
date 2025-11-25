890ff5c24e30032ee371b07a8ba59a1951a1c943
Patrik Domjen <domjen.patarik@gmail.com>
2025-11-25 20:40:00 +0100

    chore: flatten repo; move plugin files to root

--- wp-smartemailing-connector-lite/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Changelog
-
-## 1.1.0 — 2025-11-25
-- Initial public/portfolio release (vendor‑neutral).
--- wp-smartemailing-connector-lite/includes/helpers.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!function_exists('secl_render_form')) {
-    function secl_render_form($atts = []) {
-        echo do_shortcode('[smartemailing_form]');
-    }
-}
